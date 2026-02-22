23ea3b65e55f014e903440be550ad6f69cffce3d
MarcoDesidera <marcodesidera@gmail.com>
2026-02-22 15:33:31 +0100

    init spring boot

---
+++ progettospese/src/test/java/com/progettospese/progettospese/ProgettospeseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.progettospese.progettospese;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettospeseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
